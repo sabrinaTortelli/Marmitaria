de4010c2ee653991addbafed5afc1841e7a2063b
Sabrina Tortelli <sabrinaptortelli@gmail.com>
2022-02-03 11:30:28 -0300

    Base projeto.

--- src/app/food-list/food-list.component.ts
+++ src/app/food-list/food-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Food } from '../shared/food.model';
 
 @Component({
   selector: 'app-food-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodListComponent implements OnInit {
 
+  foodList: Food[] =[
+    new Food('Carne', 1),
+    new Food('Arroz', 2),
+    new Food('Couve Refogada', 2)
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
